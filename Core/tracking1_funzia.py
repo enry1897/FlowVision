8fad248aaee156342e8bdb101677afd654e07073
andreaarooo <146446661+andreaarooo@users.noreply.github.com>
2024-12-19 11:27:15 +0100

    Tracking 1 funzionante

--- Core/tracking1_funzia.py
+++ Core/tracking1_funzia.py
@@ -15,6 +15,9 @@ config = rs.config()
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
+SHOULDER_DISTANCE = 0.45  # Distanza tra le spalle in metri
+ARM_LENGTH = 0.60  # Lunghezza del braccio in metri
+
 # Avvia la pipeline
 def start_pipeline():
     try:
@@ -34,23 +37,36 @@ if not start_pipeline():
 depth_scale = pipeline.get_active_profile().get_device().first_depth_sensor().get_depth_scale()
 print(f"Depth Scale: {depth_scale} meters per unit")
 
-# Funzione per calcolare la distanza euclidea tra due punti 3D
-def calculate_distance(p1, p2):
-    return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+# Funzione per calcolare la distanza euclidea tra due punti 3D in metri date come riferimento le spalle
+def calculate_conversion_distances(p1, p2, right_shoulder, left_shoulder):
+    shoulder_distance_px = round(np.sqrt((right_shoulder[0] - left_shoulder[0])**2 + (right_shoulder[1] - left_shoulder[1])**2),3) #distanza spalle in pixel 2D
+    conversion_factor = SHOULDER_DISTANCE / shoulder_distance_px
+    #print(f"posizione polso destro x px: {p1[0]}") #Le coordinate sono a posto
+    #print(f"posizione spalla destra x px: {p2[0]}")
+    #print(f"posizione polso destro y px: {p1[1]}")
+    #print(f"posizione spalla destra y px: {p2[1]}")
+    distance_px = np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+    print(f"distance_px: {distance_px * conversion_factor}")
+    return distance_px * conversion_factor
+
 
 # Funzione principale per controllare se il braccio destro è alzato
 def is_right_arm_raised(landmarks, w, h):
     try:
         # Ottieni coordinate di spalla e polso destro
         right_shoulder = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        left_shoulder = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
         right_wrist = landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
 
         # Converti in pixel
-        shoulder_y = right_shoulder.y * h
-        wrist_y = right_wrist.y * h
+        right_shoulder_px = [right_shoulder.x * w, right_shoulder.y * h, right_shoulder.z]
+        left_shoulder_px = [left_shoulder.x * w, left_shoulder.y * h, right_shoulder.z]
+        right_wrist_px = [right_wrist.x * w, right_wrist.y * h, right_wrist.z]
+        #shoulder_y = right_shoulder_px[1]
+        #wrist_y = right_wrist.y * h
 
         # Controlla se il polso è significativamente sopra la spalla
-        if wrist_y < shoulder_y - 150:  # Range di tolleranza (50 pixel)
+        if (calculate_conversion_distances(right_wrist_px, right_shoulder_px, right_shoulder_px, left_shoulder_px) > ARM_LENGTH) & (right_wrist_px[1] + 50 < right_shoulder_px[1]):  # Range di tolleranza (150 pixel)
             return True
     except IndexError:
         pass
