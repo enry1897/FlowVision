5f6e135bab11838ce3820c68ed2f49a0f5db8857
Davide Lorenzi <15313107+DavideTommy@users.noreply.github.com>
2024-12-20 11:04:59 +0100

    [CORE] Optimized core version

--- Core/main.py
+++ Core/main.py
@@ -2,17 +2,24 @@ import cv2
 import mediapipe as mp
 import pyrealsense2 as rs
 import numpy as np
-import time 
+import time
+
+debug = False
+
+from enum import Enum
 
 # Inizializza MediaPipe
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5)
 
+w = 640
+h = 480
+
 # Inizializza RealSense Pipeline
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, w, h, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, w, h, rs.format.z16, 30)
 
 # Avvia la pipeline
 def start_pipeline():
@@ -54,14 +61,59 @@ STABILITY_WAIT_TIME = 1.0  # Tempo di attesa in secondi
 # Isteresi per il livello
 LEVEL_CHANGE_THRESHOLD = 1  # La quantità minima di cambiamento per modificare il livello
 
+#global vars
+# Inizializzazione delle variabili globali in un dizionario
+body_points = {
+    "right_shoulder": (0.0, 0.0, 0.0),
+    "left_shoulder": (0.0, 0.0, 0.0),
+    "right_hip": (0.0, 0.0, 0.0),
+    "left_hip": (0.0, 0.0, 0.0),
+    "right_wrist": (0.0, 0.0, 0.0),
+    "left_wrist": (0.0, 0.0, 0.0),
+    "right_shoulder_depth": 0.0,
+    "left_shoulder_depth": 0.0,
+    "right_wrist_depth": 0.0,
+    "left_wrist_depth": 0.0,
+    "right_shoulder_px": (0.0, 0.0, 0.0),
+    "left_shoulder_px": (0.0, 0.0, 0.0),
+    "right_wrist_px": (0.0, 0.0, 0.0),
+    "left_wrist_px": (0.0, 0.0, 0.0),
+    "left_hip_px": (0.0, 0.0, 0.0),
+}
 
+def update_body_point(body_point, new_value):
+    if body_point in body_points:
+        body_points[body_point] = new_value  # Aggiorna il valore
+    else:
+        print(f"Joint {body_point} not found!")
+
+def display_all_joints():
+    for joint, value in body_points.items():
+        print(f"{joint}: {value}")
+
+# Utilizzo TBR
+#update_body_point("right_shoulder", (1.2, 3.4, 5.6))
+#display_all_joints()
 
 
 
 
 ##FUNCTIONS
 
+def getUpdatedLandmarks(myPoseLandmarks):
+    update_body_point("right_shoulder", myPoseLandmarks.right_shoulder)
+    update_body_point("left_shoulder", myPoseLandmarks.left_shoulder)
+    update_body_point("right_hip", myPoseLandmarks.right_hip)
+    update_body_point("left_hip", myPoseLandmarks.left_hip)
+    update_body_point("right_wrist", myPoseLandmarks.right_wrist)
+    update_body_point("left_wrist", myPoseLandmarks.left_wrist)
 
+def getPixelBodyPoints():
+    update_body_point("right_shoulder_px",[body_points["right_shoulder"][0] * w, body_points["right_shoulder"][1] * h, body_points["right_shoulder"][2]])
+    update_body_point("left_shoulder_px",[body_points["left_shoulder"][0] * w, body_points["left_hip"][1] * h, body_points["right_shoulder"][2]])
+    update_body_point("right_wrist_px",[body_points["right_wrist"][0] * w, body_points["right_wrist"][1] * h, body_points["right_wrist"][2]])
+    update_body_point("left_wrist_px",[body_points["left_wrist"][0] * w, body_points["left_wrist"][1] * h, body_points["left_wrist"][2]])
+    update_body_point("left_hip_px",(int(body_points["left_hip"][0] * w), int(body_points["left_hip"][1] * h)))
 
 # Funzione per calcolare la distanza tra due punti (in pixel)
 def calculate_distance(p1, p2):
@@ -74,8 +126,9 @@ def calculate_distance_3d(p1, p2):
 
 
 # Funzione per calcolare la distanza euclidea tra due punti 3D in metri date come riferimento le spalle
-def calculate_conversion_distances(p1, p2, right_shoulder, left_shoulder):
-    shoulder_distance_px = round(np.sqrt((right_shoulder[0] - left_shoulder[0])**2 + (right_shoulder[1] - left_shoulder[1])**2),3) #distanza spalle in pixel 2D
+def calculate_conversion_distances(p1, p2):
+    global body_points
+    shoulder_distance_px = round(np.sqrt((body_points["right_shoulder_px"][0] - body_points["left_shoulder_px"][0])**2 + (body_points["right_shoulder_px"][1] - body_points["left_shoulder_px"][1])**2),3) #distanza spalle in pixel 2D
     conversion_factor = SHOULDER_DISTANCE / shoulder_distance_px
     #print(f"posizione polso destro x px: {p1[0]}") #Le coordinate sono a posto
     #print(f"posizione spalla destra x px: {p2[0]}")
@@ -86,31 +139,30 @@ def calculate_conversion_distances(p1, p2, right_shoulder, left_shoulder):
     return distance_px * conversion_factor
 
 
-
-
 # Funzione principale per controllare se il braccio destro è alzato e non è alzato il braccio sx --- TRACKING 1
-def is_right_arm_raised(my_landmarks, w, h):
+def is_right_arm_raised(my_landmarks):
     try:
+        global body_points
+
+        #getUpdatedLandmarks(my_landmarks)
         # Ottieni coordinate di spalla e polso destro
-        right_shoulder = my_landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
-        left_shoulder = my_landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
-        right_wrist = my_landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
-        left_wrist = my_landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
+        #right_shoulder = my_landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        #left_shoulder = my_landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
+        #right_wrist = my_landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
+        #left_wrist = my_landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
 
         # Converti in pixel
-        right_shoulder_px = [right_shoulder.x * w, right_shoulder.y * h, right_shoulder.z]
-        left_shoulder_px = [left_shoulder.x * w, left_shoulder.y * h, right_shoulder.z]
-        right_wrist_px = [right_wrist.x * w, right_wrist.y * h, right_wrist.z]
-        left_wrist_px = [left_wrist.x * w, left_wrist.y * h, left_wrist.z]
+        #getPixelBodyPoints()
+        #right_shoulder_px = [body_points["right_shoulder"][0] * w, body_points["right_shoulder"][1] * h, body_points["right_shoulder"][2]]
+        #left_shoulder_px = [body_points["left_shoulder"][0] * w, body_points["left_hip"][1] * h, body_points["right_shoulder"][2]]
+        #right_wrist_px = [body_points["right_wrist"][0] * w, body_points["right_wrist"][1] * h, body_points["right_wrist"][2]]
+        #left_wrist_px = [body_points["left_wrist"][0] * w, body_points["left_wrist"][1] * h, body_points["left_wrist"][2]]
         #shoulder_y = right_shoulder_px[1]
-        #wrist_y = right_wrist.y * h
+        #wrist_y = body_points["right_wrist"][1] * h
 
         # Controlla se il polso è significativamente sopra la spalla e non è alzato il braccio sx
-        if (calculate_conversion_distances(right_wrist_px, right_shoulder_px, right_shoulder_px, left_shoulder_px) > ARM_LENGTH) and (right_wrist_px[1] + 50 < right_shoulder_px[1]):  # Range di tolleranza (150 pixel)
-            if left_wrist_px[1] > left_shoulder_px[1]:
-               return True
-            else :
-                return False
+        if (calculate_conversion_distances("right_wrist_px", "right_shoulder_px") > ARM_LENGTH) and (body_points["right_wrist_px"][1] + 50 < body_points["right_shoulder_px"][1]):  # Range di tolleranza (150 pixel)
+            return body_points["left_wrist_px"][1] > ["left_shoulder_px"][1]
             
     except IndexError:
         pass
@@ -118,43 +170,44 @@ def is_right_arm_raised(my_landmarks, w, h):
 
 
 
-
-
 # Funzione per rilevare se le mani sono sovrapposte sul cuore --- TRACKING 2 - CUORE
-def check_hands_on_heart(pose_landmarks, w, h):
+def check_hands_on_heart(pose_landmarks):
     try:
+        global body_points
+
         # Coordinate chiave: spalle, polsi
-        right_shoulder = pose_landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
-        left_shoulder = pose_landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
-        right_wrist = pose_landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
-        left_wrist = pose_landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
-        left_hip = pose_landmarks[mp_pose.PoseLandmark.LEFT_HIP]
+        #update coordinates:
+        #getUpdatedLandmarks(pose_landmarks)
+
+        #right_shoulder = pose_landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        #left_shoulder = pose_landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
+        #right_wrist = pose_landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
+        #left_wrist = pose_landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
+        #left_hip = pose_landmarks[mp_pose.PoseLandmark.LEFT_HIP]
 
         # Converti in pixel
-        right_shoulder_px = (int(right_shoulder.x * w), int(right_shoulder.y * h))
-        left_shoulder_px = (int(left_shoulder.x * w), int(left_shoulder.y * h))
-        right_wrist_px = (int(right_wrist.x * w), int(right_wrist.y * h))
-        left_wrist_px = (int(left_wrist.x * w), int(left_wrist.y * h))
-        left_hip_px = (int(left_hip.x * w), int(left_hip.y * h))
+        #getPixelBodyPoints()
+        #right_shoulder_px = (int(body_points["right_shoulder"][0] * my_w), int(body_points["right_shoulder"][1] * my_h))
+        #left_shoulder_px = (int(body_points["left_shoulder"][0] * my_w), int(body_points["left_hip"][1] * my_h))
+        #right_wrist_px = (int(body_points["right_wrist"][0] * my_w), int(body_points["right_wrist"][1] * my_h))
+        #left_wrist_px = (int(body_points["left_wrist"][0] * my_w), int(body_points["left_wrist"][1] * my_h))
+        #left_hip_px = (int(body_points["left_hip"][0] * my_w), int(body_points["left_hip"][1] * my_h))
 
-        if right_shoulder.y < right_wrist.y < left_hip.y:
+        if body_points["right_shoulder"][1] < body_points["right_wrist"][1] < body_points["left_hip"][1]:
             # Definiamo la zona del cuore come il centro tra le spalle
             #print("calcolo cuore")
-            heart_x = (right_shoulder.x + left_shoulder.x) / 2
-            heart_y = right_shoulder.y - ((right_shoulder.y - left_hip.y) / 4)  # Posizione tra le spalle, nella zona del petto
+            heart_x = (body_points["right_shoulder"][0] + body_points["left_shoulder"][0]) / 2
+            heart_y = body_points["right_shoulder"][1] - ((body_points["right_shoulder"][1] - body_points["left_hip"][1]) / 4)  # Posizione tra le spalle, nella zona del petto
 
             # Zona del cuore in pixel
             heart_region = (int(heart_x * w), int(heart_y * h))
 
             # Calcola la distanza tra le mani e la zona del cuore
-            right_wrist_distance = calculate_distance((right_wrist.x * w, right_wrist.y * h), heart_region)
-            left_wrist_distance = calculate_distance((left_wrist.x * w, left_wrist.y * h), heart_region)
+            right_wrist_distance = calculate_distance((body_points["right_wrist"][0] * w, body_points["right_wrist"][1] * h), heart_region)
+            left_wrist_distance = calculate_distance((body_points["left_wrist"][0] * w, body_points["left_wrist"][1] * h), heart_region)
 
             # Se entrambe le mani sono abbastanza vicine alla zona del cuore, attiviamo una funzione
-            if right_wrist_distance < HEART_REGION_TOLERANCE * w and left_wrist_distance < HEART_REGION_TOLERANCE * w:
-                return True  # Le mani sono sovrapposte al cuore
-            else:
-                return False
+            return right_wrist_distance < HEART_REGION_TOLERANCE * w and left_wrist_distance < HEART_REGION_TOLERANCE * h
 
     except IndexError:
         pass
@@ -163,34 +216,37 @@ def check_hands_on_heart(pose_landmarks, w, h):
 
 
 # Funzione per calcolare il livello proporzionale --- TRACKING 3 -- CO2
-def calculate_level(pose_landmarks, my_w, my_h, my_depth_image):
-    global level, prev_level
+def calculate_level(my_pose_landmarks, my_depth_image):
+    global body_points,prev_level
     try:
+        #getUpdatedLandmarks(my_pose_landmarks) #verificare se da chiamare una volta sola nel main loop
+
         # Coordinate chiave: spalle, anche, polsi
-        right_shoulder = pose_landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
-        left_shoulder = pose_landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
-        right_hip = pose_landmarks[mp_pose.PoseLandmark.RIGHT_HIP]
-        left_hip = pose_landmarks[mp_pose.PoseLandmark.LEFT_HIP]
-        right_wrist = pose_landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
-        left_wrist = pose_landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
+        #right_shoulder = pose_landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        #left_shoulder = pose_landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
+        #right_hip = pose_landmarks[mp_pose.PoseLandmark.RIGHT_HIP]
+        #left_hip = pose_landmarks[mp_pose.PoseLandmark.LEFT_HIP]
+        #right_wrist = pose_landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
+        #left_wrist = pose_landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
 
         # Converti in pixel
-        right_shoulder_px = (int(right_shoulder.x * my_w), int(right_shoulder.y * my_h))
-        left_shoulder_px = (int(left_shoulder.x * my_w), int(left_shoulder.y * my_h))
-        right_wrist_px = (int(right_wrist.x * my_w), int(right_wrist.y * my_h))
-        left_wrist_px = (int(left_wrist.x * my_w), int(left_wrist.y * my_h))
+        #getPixelBodyPoints()
+        #right_shoulder_px = (int(body_points["right_shoulder"][0] * w), int(body_points["right_shoulder"][1] * h))
+        #left_shoulder_px = (int(body_points["left_shoulder"][0] * w), int(body_points["left_hip"][1] * h))
+        #right_wrist_px = (int(body_points["right_wrist"][0] * w), int(body_points["right_wrist"][1] * h))
+        #left_wrist_px = (int(body_points["left_wrist"][0] * w), int(body_points["left_wrist"][1] * h))
 
         # Ottieni profondità dai frame di profondità
-        right_shoulder_depth = my_depth_image[right_shoulder_px[1], right_shoulder_px[0]] * depth_scale
-        left_shoulder_depth = my_depth_image[left_shoulder_px[1], left_shoulder_px[0]] * depth_scale
-        right_wrist_depth = my_depth_image[right_wrist_px[1], right_wrist_px[0]] * depth_scale
-        left_wrist_depth = my_depth_image[left_wrist_px[1], left_wrist_px[0]] * depth_scale
+        right_shoulder_depth = my_depth_image[body_points["right_shoulder_px"][1], body_points["right_shoulder_px"][0]] * depth_scale
+        left_shoulder_depth = my_depth_image[body_points["left_shoulder_px"][1], body_points["left_shoulder_px"][0]] * depth_scale
+        right_wrist_depth = my_depth_image[body_points["right_wrist_px"][1], body_points["right_wrist_px"][0]] * depth_scale
+        left_wrist_depth = my_depth_image[body_points["right_left_px"][1], body_points["right_left_px"][0]] * depth_scale
 
         # Coordinate 3D
-        right_shoulder_3d = (right_shoulder.x, right_shoulder.y, right_shoulder_depth)
-        left_shoulder_3d = (left_shoulder.x, left_shoulder.y, left_shoulder_depth)
-        right_wrist_3d = (right_wrist.x, right_wrist.y, right_wrist_depth)
-        left_wrist_3d = (left_wrist.x, left_wrist.y, left_wrist_depth)
+        right_shoulder_3d = (body_points["right_shoulder"][0], body_points["right_shoulder"][1], right_shoulder_depth)
+        left_shoulder_3d = (body_points["left_shoulder"][0], body_points["left_hip"][1], left_shoulder_depth)
+        right_wrist_3d = (body_points["right_wrist"][0], body_points["right_wrist"][1], right_wrist_depth)
+        left_wrist_3d = (body_points["left_wrist"][0], body_points["left_wrist"][1], left_wrist_depth)
 
         # Calcola la distanza 3D tra spalla e polso
         right_arm_length = calculate_distance_3d(right_shoulder_3d, right_wrist_3d)
@@ -198,28 +254,28 @@ def calculate_level(pose_landmarks, my_w, my_h, my_depth_image):
 
         # Verifica che entrambe le braccia siano distese
         if right_arm_length < ARM_MIN_LENGTH or left_arm_length < ARM_MIN_LENGTH:
-            level = 0
+            myLevel = 0
             return
 
         # Altezza media delle mani
-        avg_hand_height = (right_wrist.y + left_wrist.y) / 2
+        avg_hand_height = (body_points["right_wrist"][1] + body_points["left_wrist"][1]) / 2
 
         # Altezza media delle spalle e delle anche
-        avg_shoulder_height = (right_shoulder.y + left_shoulder.y) / 2
-        avg_hip_height = (right_hip.y + left_hip.y) / 2
+        avg_shoulder_height = (body_points["right_shoulder"][1] + body_points["left_hip"][1]) / 2
+        avg_hip_height = (body_points["right_hip"][1] + body_points["left_hip"][1]) / 2
 
         # Calcolo della differenza di altezza tra i polsi
-        wrist_height_diff = abs(right_wrist.y - left_wrist.y)
+        wrist_height_diff = abs(body_points["right_wrist"][1] - body_points["left_wrist"][1])
 
         # Se non rileva entrambe le mani, il livello non può essere incrementato
         if not (right_arm_length >= ARM_MIN_LENGTH and left_arm_length >= ARM_MIN_LENGTH):
-            level = 0
-            prev_level = level
+            myLevel = 0
+            prev_level = myLevel
             return
 
         # Condizioni per aumentare il livello: entrambe le braccia devono essere sollevate sopra le anche,
         # distese, e le mani devono essere a livello simile
-        if right_wrist.y < avg_hip_height and left_wrist.y < avg_hip_height:  # Le mani sono sopra le anche
+        if body_points["right_wrist"][1] < avg_hip_height and body_points["left_wrist"][1] < avg_hip_height:  # Le mani sono sopra le anche
             if wrist_height_diff < HAND_HEIGHT_TOLERANCE:  # Le mani sono a livelli simili
                 # Calcola il livello in base all'altezza delle mani rispetto alle anche
                 normalized_height = (avg_hip_height - avg_hand_height) / (avg_hip_height - avg_shoulder_height * 0.5)
@@ -233,8 +289,8 @@ def calculate_level(pose_landmarks, my_w, my_h, my_depth_image):
 
         # Applicare l'isteresi: il livello cambierà solo se la differenza con il livello precedente è significativa
         if abs(new_level - prev_level) >= LEVEL_CHANGE_THRESHOLD:
-            level = min(new_level, max_level)  # Tronca il livello a max_level (10)
-            prev_level = level
+            myLevel = min(new_level, max_level)  # Tronca il livello a max_level (10)
+            prev_level = myLevel
 
 
     except IndexError:
@@ -270,10 +326,13 @@ try:
             mp.solutions.drawing_utils.draw_landmarks(
                 color_image, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
+            #update all local datas
+            getUpdatedLandmarks(pose_results.pose_landmarks)
+            getPixelBodyPoints()
 
             heart = 0
             # Controlla se le mani sono sovrapposte al cuore
-            if check_hands_on_heart(pose_results.pose_landmarks.landmark, w, h):
+            if check_hands_on_heart(pose_results.pose_landmarks.landmark):
                 # Mostra il messaggio se le mani sono sovrapposte al cuore
                 heart = 1
                 
